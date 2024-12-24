c1ff4735942160ef864a74321c9155c8f3b10e8e
Johnvilin-Sibina <johnvilinsibina@gmail.com>
2024-12-24 08:01:57 +0530

    Signup page is connected with backend.

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,8 +1,51 @@
-import { Button, Label, TextInput } from "flowbite-react";
-import React from "react";
-import { Link } from "react-router-dom";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { HiInformationCircle } from "react-icons/hi";
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const navigate = useNavigate();
+
+  //Function to handle the value change in the form
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  //Function to handle form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage("Please fill out all the fields");
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const response = await fetch(
+        "http://localhost:5000/api/auth/register-user",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+      const data = await response.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      if (response.ok) {
+        navigate("/signin");
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
@@ -12,28 +55,66 @@ const SignUp = () => {
               LootMart
             </span>
           </div>
-          <p className="text-sm mt-6">Sign up and create an account to place your orders. Use your email and password or Google to Sign up. **This is a demo project.""</p>
+          <p className="text-sm mt-6">
+            Sign up and create an account to place your orders. Use your email
+            and password or Google to Sign up. **This is a demo project.""
+          </p>
         </div>
         <div className="flex-1">
-            <form className="flex flex-col gap-4">
-                <div>
-                    <Label value='User Name' />
-                    <TextInput type='text' placeholder="Enter your user name" id='username' />
-                </div>
-                <div>
-                    <Label value='Email' />
-                    <TextInput type='email' placeholder="Enter your email" id='email' />
-                </div>
-                <div>
-                    <Label value='Password' />
-                    <TextInput type='password' placeholder="Enter your password" id='password' />
-                </div>
-                <Button className="bg-gradient-to-r from-emerald-300 to-emerald-500" type="submit">Sign Up</Button>
-            </form>
-            <div className="flex gap-2 text-sm mt-6">
-                <span>Already have an account?</span>
-                <Link to="/signin" className="text-blue-700 font-semibold">Sign In</Link>
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+            <div>
+              <Label value="User Name" />
+              <TextInput
+                type="text"
+                placeholder="Enter your user name"
+                id="username"
+                onChange={handleChange}
+              />
             </div>
+            <div>
+              <Label value="Email" />
+              <TextInput
+                type="email"
+                placeholder="Enter your email"
+                id="email"
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <Label value="Password" />
+              <TextInput
+                type="password"
+                placeholder="Enter your password"
+                id="password"
+                onChange={handleChange}
+              />
+            </div>
+            <Button
+              className="bg-gradient-to-r from-emerald-300 to-emerald-500"
+              type="submit"
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <Spinner color="success" />
+                  <span className='pl-3'>Loading...</span>
+                </>
+              ) : (
+                "Sign Up"
+              )}
+            </Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-6">
+            <span>Already have an account?</span>
+            <Link to="/signin" className="text-blue-700 font-semibold">
+              Sign In
+            </Link>
+          </div>
+          {errorMessage && (
+            <Alert className="mt-3" color="failure" icon={HiInformationCircle} withBorderAccent>
+              <span className="font-medium me-2">OOPS!</span>{errorMessage}
+              </Alert>
+          )}
         </div>
       </div>
     </div>
