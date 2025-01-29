9835fbb4ed8c5c1d4a7729cd60e5a7b1e0f185ee
Johnvilin-Sibina <johnvilinsibina@gmail.com>
2025-01-29 13:22:14 +0530

    handleRemoveFromCart updated.

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,7 +1,7 @@
 import { Button } from "flowbite-react";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {loadStripe} from '@stripe/stripe-js';
+import { loadStripe } from "@stripe/stripe-js";
 
 const Cart = () => {
   const [cartItems, setCartItems] = useState([]);
@@ -60,7 +60,6 @@ const Cart = () => {
     }
   };
 
-  
   const handleRemoveFromCart = async (productId) => {
     try {
       const response = await fetch(
@@ -81,6 +80,12 @@ const Cart = () => {
       }
 
       await fetchCartDetails();
+
+      if (cartItems.length === 1) {
+        setCartItems([]); // Ensure re-render
+        setSubtotal(0);
+      }
+
     } catch (error) {
       console.log(error.message);
     }
@@ -90,30 +95,35 @@ const Cart = () => {
     fetchCartDetails();
   }, []);
 
-  const makePayment = async()=>{
+  const makePayment = async () => {
     const stripe = await loadStripe(import.meta.env.VITE_STRIPE_PUB_KEY);
-try {
-  const response = await fetch('http://localhost:5000/api/user/create-checkout-session',{
-    method:'POST',
-    headers:{
-      'Content-Type':'application/json',
-      token:localStorage.getItem('Token')
-    },
-    body:JSON.stringify({products:cartItems})
-  })
-  const session = await response.json()
+    try {
+      const response = await fetch(
+        "http://localhost:5000/api/user/create-checkout-session",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            token: localStorage.getItem("Token"),
+          },
+          body: JSON.stringify({ products: cartItems, user: currentUser.rest }),
+        }
+      );
+      const session = await response.json();
 
-  const result = stripe.redirectToCheckout({
-    sessionId:session.id
-  })
-} catch (error) {
-  console.log(error)
-}
-  }
+      const result = stripe.redirectToCheckout({
+        sessionId: session.id,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="min-h-screen w-screen p-8">
       <div className="max-w-4xl mx-auto bg-white shadow-md rounded-lg p-6">
-        <h1 className="text-3xl  text-emerald-700 font-serif font-semibold mb-4 text-center">My Cart</h1>
+        <h1 className="text-3xl  text-emerald-700 font-serif font-semibold mb-4 text-center">
+          My Cart
+        </h1>
         {cartItems.length === 0 ? (
           <p className="text-gray-600">Your cart is empty.</p>
         ) : (
@@ -135,7 +145,7 @@ try {
                     </p>
                     <p className="text-sm text-gray-600">{item.description}</p>
                     <p className="text-gray-500">
-                     <b> ${item.price.toFixed(2)}</b>
+                      <b> ${item.price.toFixed(2)}</b>
                     </p>
                     <div className="flex items-center gap-2 mt-2">
                       <button
@@ -178,7 +188,9 @@ try {
               </p>
             </div>
             <div className="flex justify-end mt-5">
-              <Button type='button' color='blue' onClick={makePayment}>Pay</Button>
+              <Button type="button" color="blue" onClick={makePayment}>
+                Pay
+              </Button>
             </div>
           </>
         )}
